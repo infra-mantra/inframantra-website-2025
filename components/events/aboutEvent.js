7039eac0bb55b17e5115c46c714150c6257f1fa4
Rohit Kumar <rohit.kumar@inframantra.com>
2026-05-04 18:22:20 +0530

    mr at garvit

--- components/events/aboutEvent.js
+++ components/events/aboutEvent.js
@@ -19,8 +19,8 @@ function aboutEvent() {
               <span className={styles.highlightName}>
                 Garvit Tiwari
               </span>{" "} */}
-             At the Expo, you’ll get the opportunity to meet our Founder & Director, Mr. Shiwang Suraj and Co-Founder & Director,s Mr.Garvit Tiwari, along with senior executives from some of India’s most reputed developers like Godrej Properties, Tulip Infratech, Whiteland Corporation, and more. We’re bringing a curated selection of premium luxury residences, giving you direct access to high-potential opportunities in India’s dynamic real estate market.
-    
+             At the Expo, you’ll get the opportunity to meet our Founder & Director, Mr. Shiwang Suraj and Co-Founder & Director, Mr. Garvit Tiwari, along with senior executives from some of India’s most reputed developers like Godrej Properties, Tulip Infratech, Whiteland Corporation, and more. We’re bringing a curated selection of premium luxury residences, giving you direct access to high-potential opportunities in India’s dynamic real estate market.
+     
               <br /><br />
     
              This Expo goes beyond property discovery, it’s about making confident, well-informed investment decisions. You’ll also gain clarity on key aspects of investing in India, including repatriation of funds, ensuring a seamless and transparent investment journey.
